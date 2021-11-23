e62898aec785e1c1a1930a89bcb3a7d0e21ee401
SlejmUr <52343819+SlejmUr@users.noreply.github.com>
2021-11-23 23:52:08 +0000

    Update INI.cs
    "Data\\Config\\Config.ini" => ConfigINI
    IsDebug added (True/False)
    Added two functions for future to "GetFromINI" and for "SetToINI"

--- AIO-Tools/Classes/INI.cs
+++ AIO-Tools/Classes/INI.cs
@@ -5,13 +5,16 @@ namespace AIO_Tools.Classes
 {
     class INI
     {
+        #region String
+        private static string ConfigINI = "Data\\Config\\Config.ini";
+        #endregion
         #region Config
         public static void SetUserName(string _username)
         {
             var parserdata = new FileIniDataParser();
-            IniData data = parserdata.ReadFile("Data\\Config\\Config.ini");
+            IniData data = parserdata.ReadFile(ConfigINI);
             data["User"]["name"] = _username;
-            parserdata.WriteFile("Data\\Config\\Config.ini", data);
+            parserdata.WriteFile(ConfigINI, data);
         }
         public static string UserName;
         public string GetUserName()
@@ -81,6 +84,31 @@ namespace AIO_Tools.Classes
             IniData data = parserdata.ReadFile("Data\\Config\\Config.ini");
             data["User"]["tempdownloaded"] = _tempdownloaded;
             parserdata.WriteFile("Data\\Config\\Config.ini", data);
+        }
+        public bool IsDebug()
+        {
+            var parserdata = new FileIniDataParser();
+            IniData data = parserdata.ReadFile(ConfigINI);
+            string Debug_Str = data["GeneralConfiguration"]["Debug"];
+            bool B_Debug = bool.Parse(Debug_Str);
+            return B_Debug;
+        }
+        #endregion
+        #region Private //Testing
+        private string GetFromINI(string Category, string Name)
+        {
+            var parserdata = new FileIniDataParser();
+            IniData data = parserdata.ReadFile(ConfigINI);
+            string ReadedStuff = data[Category][Name];
+            return ReadedStuff;
+        }
+
+        private void SetToINI(string Category, string Name,string Data)
+        {
+            var parserdata = new FileIniDataParser();
+            IniData data = parserdata.ReadFile(ConfigINI);
+            data[Category][Name] = Data;
+            parserdata.WriteFile(ConfigINI, data);
         }
         #endregion
     }
