4294b10763e6c0786a7d91bb907557e3aa8e8294
SlejmUr <52343819+SlejmUr@users.noreply.github.com>
2021-11-24 12:01:43 +0000

    DebugLog , and Strings
    DebugLog Added, same as "SpecificLog"
    "Data\\path.txt" => TXTPath
    "Data\\LOGS\\logging.txt" => LoggingTXT

--- AIO-Tools/Classes/logging.cs
+++ AIO-Tools/Classes/logging.cs
@@ -1,44 +1,60 @@
 ﻿using System;
 using System.IO;
+using AIO_Tools.Classes;
 
 namespace AIO_Tools
 {
     class Logging
     {
         #region Logging
+        private static string LoggingTXT = "Data\\LOGS\\logging.txt"
         public static void WriteLog(string strLog)
         {
-            FileInfo logFileInfo = new FileInfo("Data\\LOGS\\logging.txt");
+            FileInfo logFileInfo = new FileInfo(LoggingTXT);
             DirectoryInfo logDirInfo = new DirectoryInfo(logFileInfo.DirectoryName);
             if (!logDirInfo.Exists) logDirInfo.Create();
-            using FileStream fileStream = new FileStream("Data\\LOGS\\logging.txt", FileMode.Append);
+            using FileStream fileStream = new FileStream(LoggingTXT, FileMode.Append);
             using StreamWriter log = new StreamWriter(fileStream);
             log.WriteLine(DateTime.Now + " | INFO | " + strLog);
         }
         public static void SpecificLog(string strLog, string logType)
         {
-            FileInfo logFileInfo = new FileInfo("Data\\LOGS\\logging.txt");
+            FileInfo logFileInfo = new FileInfo(LoggingTXT);
             DirectoryInfo logDirInfo = new DirectoryInfo(logFileInfo.DirectoryName);
             if (!logDirInfo.Exists) logDirInfo.Create();
-            using FileStream fileStream = new FileStream("Data\\LOGS\\logging.txt", FileMode.Append);
+            using FileStream fileStream = new FileStream(LoggingTXT, FileMode.Append);
             using StreamWriter log = new StreamWriter(fileStream);
             log.WriteLine(DateTime.Now + " | " + logType + " | " + strLog);
         }
+        public static void DebugLog(string strLog, string logType)
+        {
+            INI ini = new INI();
+            if (ini.IsDebug() == True)
+            {
+                FileInfo logFileInfo = new FileInfo(LoggingTXT);
+                DirectoryInfo logDirInfo = new DirectoryInfo(logFileInfo.DirectoryName);
+                if (!logDirInfo.Exists) logDirInfo.Create();
+                using FileStream fileStream = new FileStream(LoggingTXT, FileMode.Append);
+                using StreamWriter log = new StreamWriter(fileStream);
+                log.WriteLine("[DEBUG]" + DateTime.Now + " | " + logType + " | " + strLog);
+            }
+        }
         #endregion
         #region Write Path txt
+        private static string TXTPath = "Data\\path.txt"
         public static void PathTXT(string path)
         {
-            if (File.Exists("Data\\path.txt"))
+            if (File.Exists(TXTPath))
             {
-                File.Delete("Data\\path.txt");
+                File.Delete(TXTPath);
             }
-            FileInfo logFileInfo = new FileInfo("Data\\path.txt");
+            FileInfo logFileInfo = new FileInfo(TXTPath);
             DirectoryInfo logDirInfo = new DirectoryInfo(logFileInfo.DirectoryName);
             if (!logDirInfo.Exists)
             {
                 logDirInfo.Create();
             }
-            using FileStream fileStream = new FileStream("Data\\path.txt", FileMode.Append);
+            using FileStream fileStream = new FileStream(TXTPath, FileMode.Append);
             using StreamWriter log = new StreamWriter(fileStream);
             log.WriteLine(path);
         }
