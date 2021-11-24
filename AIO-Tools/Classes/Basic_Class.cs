ceb6999a6943e287cd19d6c1158d36dffbfb311f
SlejmUr <52343819+SlejmUr@users.noreply.github.com>
2021-11-24 12:26:45 +0000

    Dev version
    DEV-2021112X.X

--- AIO-Tools/Classes/Basic_Class.cs
+++ AIO-Tools/Classes/Basic_Class.cs
@@ -14,7 +14,7 @@ namespace AIO_Tools.Classes
         "Mute","shadowlegacy","GrandLarceny","roadtosi2021",
         "CrimsonHeist","northstar","CrystalGuard","rainbowismagic",
         "evn_quarantine","Y6S3_EVN_MIX","Placeholder","Placeholder2","base"};
-        public static string Version = "20211028.0";
+        public static string Version = "DEV-2021112X.X";
         private static readonly string extractPath = Directory.GetCurrentDirectory();
         public static readonly string datasDB = @"URI=file:" + extractPath + "\\Data\\DataBase\\datas.db";
         public static readonly string allDB = @"URI=file:" + extractPath + "\\Data\\DataBase\\all.db";
