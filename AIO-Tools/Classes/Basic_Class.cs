97596b98b66244c79fb1f14ad078510b7ec0341c
SlejmUr <52343819+SlejmUr@users.noreply.github.com>
2021-12-09 12:37:05 +0100

    20211209.0 11 is November 12 is December :)

--- AIO-Tools/Classes/Basic_Class.cs
+++ AIO-Tools/Classes/Basic_Class.cs
@@ -14,7 +14,7 @@ namespace AIO_Tools.Classes
         "Mute","shadowlegacy","GrandLarceny","roadtosi2021",
         "CrimsonHeist","northstar","CrystalGuard","rainbowismagic",
         "evn_quarantine","Y6S3_EVN_MIX","highcalibre","Placeholder","Placeholder2","base"};
-        public static string Version = "20211109.0";
+        public static string Version = "20211209.0";
         private static readonly string extractPath = Directory.GetCurrentDirectory();
         public static readonly string datasDB = @"URI=file:" + extractPath + "\\Data\\DataBase\\datas.db";
         public static readonly string allDB = @"URI=file:" + extractPath + "\\Data\\DataBase\\all.db";
